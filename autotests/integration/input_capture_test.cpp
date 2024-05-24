331cb75fd0ecbd0c977999559352b63932063dc0
David Redondo <kde@david-redondo.de>
2024-05-24 17:05:51 +0200

    throw away events

--- autotests/integration/input_capture_test.cpp
+++ autotests/integration/input_capture_test.cpp
@@ -246,6 +246,7 @@ void TestInputCapture::testInputCapture()
 
     QVERIFY(eiReadableSpy.wait());
     ei_dispatch(ei);
+    while (ei_get_event(ei)) { }
     eiReadableSpy.clear();
 
     qDebug() << "--------------------------------------------------";
@@ -263,7 +264,8 @@ void TestInputCapture::testInputCapture()
     if (!eiReadableSpy.empty()) {
         // On FreeBSD CI the socketnotifier activates but there are no events!?
         ei_dispatch(ei);
-        qDebug() << ei_peek_event(ei) <<  QTest::toString(ei_event_get_type(ei_peek_event(ei)));
+        if (ei_peek_event(ei))
+            qDebug() << ei_peek_event(ei) << QTest::toString(ei_event_get_type(ei_peek_event(ei)));
         QVERIFY2(!ei_peek_event(ei), QTest::toString(ei_event_get_type(ei_peek_event(ei))));
     }
 
